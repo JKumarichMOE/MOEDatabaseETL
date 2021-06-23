806e0d16f11c186d7eb3ef9060ae3d3375895732
Jonathan Kumarich <jonathan.kumarich@education.govt.nz>
2021-06-24 10:11:53 +1200

    connect_db initial function skeleton

--- R/connect_db.R
+++ R/connect_db.R
@@ -1,1 +1,14 @@
-odbc::odbc()
+connect_db <- function(server="learnerbds.db.moe.govt.nz", database="LearnerBDS") {
+
+  conn <- DBI::dbConnect(
+    odbc::odbc(),
+    Driver   = "SQL Server",
+    Server   = server,
+    Database = database,
+    Port     = 1433)
+
+  return(conn)
+}
+
+
+
